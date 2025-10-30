aa4ddee363655cd6138bdc541542d6589d2db405
leblaluc <lucasleblanc30@gmail.com>
2025-10-30 08:52:09 -0400

    rev 2 local

--- src/main/java/midtermgitcode2/midtermgitcode2/MidtermGitCode2.java
+++ src/main/java/midtermgitcode2/midtermgitcode2/MidtermGitCode2.java
@@ -11,6 +11,6 @@ package midtermgitcode2.midtermgitcode2;
 public class MidtermGitCode2 {
 
     public static void main(String[] args) {
-        System.out.println("Midterm Git Test rev1");
+        System.out.println("Midterm Git Test rev2");
     }
 }
